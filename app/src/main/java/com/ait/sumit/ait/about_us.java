f10dd1d21ef0f0ea243bd913b8ccb0a47b13eaa0
grace5921 <androidlover5842@gmail.com>
2016-06-16 12:22:35 +0530

    add about us page

--- app/src/main/java/com/ait/sumit/ait/about_us.java
+++ app/src/main/java/com/ait/sumit/ait/about_us.java
@@ -2,12 +2,21 @@ package com.ait.sumit.ait;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class about_us extends AppCompatActivity {
+
+    String about="about.html";
+    String asset = "file:///android_asset/";
+    String getAbout=asset+about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        WebView mywebview = (WebView) this.findViewById(R.id.webView2);
+        mywebview.getSettings() .setJavaScriptCanOpenWindowsAutomatically(true);
+        mywebview.loadUrl(getAbout);
     }
+
 }
